8f72da93c5bd3f27338af177e801bbc7ac984dde
jony <ravshanovjony7@gmail.com>
2025-08-14 16:55:20 +0500

    8-dars Nvbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom"
-import {Main, Login, Register} from "./compnents"
+import {Main, Login, Register, Navbar} from "./compnents"
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element = {<Main/>}/>
-        <Route path="/" element = {<Login/>}/>
-        <Route path="/" element = {<Register/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/register" element = {<Register/>}/>
       </Routes>
     </div>
   )
---
+++ src/compnents/constants/index.js
@@ -0,0 +1,2 @@
+export const logo = "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/42/ce/76/42ce769a-d334-ab5a-ccab-636527a93478/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/434x0w.webp";
+export const icon = "https://media.graphassets.com/BldVa3tyRDy2QkoyQq9R";
